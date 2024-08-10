1d677941eb2a20f06e7f967a807b7a1de50859fa
macwille <vw.manninen@gmail.com>
2024-08-11 02:41:43 +0300

    remove Main from github

--- src/main/java/com/github/macwille/chess/Main.java
+++
@@ -1,8 +0,0 @@
-package com.github.macwille.chess;
-
-public class Main {
-    public static void main(String[] args) {
-        TerminalUI ui = new TerminalUI(System.in);
-        ui.start();
-    }
-}
